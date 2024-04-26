c32259db801e9bdcf9094f0ea2bb63b662d820eb
ahrimaz <eric.stanard@gmail.com>
2024-04-26 12:00:32 -0400

    navbar styling, color gradient bg

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-2 fixed top-0 left-0 w-full shadow-lg z-10">
+    <nav className="bg-gradient-to-b from-slate-800 to-slate-700 text-white p-2 fixed top-0 left-0 w-full shadow-lg z-10">
       <div className="container mx-auto flex justify-between items-center">
         <div>
           <Link href="/" className="text-xl font-bold">Home</Link>
