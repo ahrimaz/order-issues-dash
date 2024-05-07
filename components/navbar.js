7b807d1a2c1866d81dfb68804e4f447a1d3323db
ahrimaz <eric.stanard@gmail.com>
2024-05-07 01:05:40 -0400

    fixed login flicker on new page, learned import of usesession

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import Login from './login';
+import { useSession } from 'next-auth/react';
 
 const Navbar = () => {
+
+  const { data: session } = useSession();
+
   return (
     <nav className="bg-gradient-to-b from-slate-800 to-slate-700 text-white p-2 fixed top-0 left-0 w-full shadow-lg z-10">
       <div className="container mx-auto flex justify-between items-center">
@@ -20,8 +24,8 @@ const Navbar = () => {
           <Link href="/contact">
             <span className="ml-4 cursor-pointer">Contact</span>
           </Link>
-          <div className="ml-4"> {/* wraplogin component */}
-            <Login/>
+          <div className="ml-4">
+            {session ? session.user.name : <Login/>}
           </div>
         </div>
       </div>
