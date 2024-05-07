b6d1528afb973fe88be26f7c006c7563a2af52b7
ahrimaz <eric.stanard@gmail.com>
2024-05-07 17:31:23 -0400

    search function, styling

--- pages/pricelist.js
+++ pages/pricelist.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 const PriceList = () => {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -13,17 +14,30 @@ const PriceList = () => {
         fetchProducts();
     }, []);
 
+    const filteredProducts = products.filter(product =>
+        product.product.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-            {products.map((product) => (
-                <div key={product._id.$oid} className="border border-gray-200 rounded-lg p-4 shadow-sm">
-                    <h2 className="text-lg font-semibold mb-2">{product.product}</h2>
-                    <p className="text-gray-500">Direct Price: {product.directPrice.$numberDecimal}</p>
-                    <p className="text-gray-500">LC Price: {product.lcPrice.$numberDecimal}</p>
-                    <p className="text-gray-500">VPP Price: {product.vppPrice.$numberDecimal}</p>
-                    <p className="text-gray-500">VUP Price: {product.vupPrice.$numberDecimal}</p>
-                </div>
-            ))}
+        <div className="pt-16 mx-auto max-w-4xl">
+            <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+                className="w-full p-2 mb-4 border border-gray-200 rounded transition-all duration-200 ease-in-out" // Add transition here
+                placeholder="Search for a product..."
+            />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+                {filteredProducts.map((product) => (
+                    <div key={product._id.$oid} className="border border-gray-200 rounded-lg p-4 shadow-sm">
+                        <h2 className="text-lg font-semibold mb-2">{product.product}</h2>
+                        <p className="text-gray-500">Direct Price: {product.directPrice.$numberDecimal}</p>
+                        <p className="text-gray-500">LC Price: {product.lcPrice.$numberDecimal}</p>
+                        <p className="text-gray-500">VPP Price: {product.vppPrice.$numberDecimal}</p>
+                        <p className="text-gray-500">VUP Price: {product.vupPrice.$numberDecimal}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
